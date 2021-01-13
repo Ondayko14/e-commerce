512fcac2fa10f435c3ee5f6973087a67260ed643
Brandon O <b.ondayko@gmail.com>
2021-01-13 17:25:28 -0600

    The put route has been added for Cats

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -64,6 +64,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({message: 'Could not find Categorie'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
