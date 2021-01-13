32092bfbe989215999a4b7e59e184a98b8d3e2a6
Brandon O <b.ondayko@gmail.com>
2021-01-13 16:05:06 -0600

    The models have been restructured and seeded

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,28 +9,34 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    id: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  {
-    Product_name: DataTypes.STRING,
-    allowNull: false,
-  },
-  {
-    price: DataTypes.INTEGER,
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+  
+    Product_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  
+    price:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+  
+    stock:{ 
+    type: DataTypes.INTEGER,
     allowNull: false
-  },
-  {
-    stock: DataTypes.INTEGER,
-    allowNull: false
-  },
-  {
-    category_id: DataTypes.INTEGER,
-    references: {
-      model: 'category',
-      key: 'id'
+    },
+  
+    category_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,8 +6,32 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
-  },
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+  
+    product_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      refrences: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+  
+  
+    tag_id:{
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      refrences: {
+        model: 'tag',
+        key: 'id'
+        }
+      }
+    },
   {
     sequelize,
     timestamps: false,
